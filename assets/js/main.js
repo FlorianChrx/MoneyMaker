7ba3694dca20861a82cc958d51683f4ae347ed8f
vaniscotte-geoffrey <vaniscotte.geoffrey@hotmail.com>
2020-02-18 18:12:18 +0100

    Fix Calcul

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,31 @@
-const button = document.getElementById('button');
-button.onclick = function calcul(){
+let action = function calcul(){
+
+    // Recupération des valeurs du formulaire
     const prix_achat = document.getElementById('prix_achat').value;
     const quantite_achetee = document.getElementById('quantite_achetee').value;
     const prix_vente = document.getElementById('prix_vente').value;
 
 
-    const pourcentage = prix_vente > prix_achat ? (prix_vente / prix_achat - 1) * 100 : -((prix_achat / prix_vente - 1) * 100);
-
+    // Calcul 
+    const pourcentage = (prix_vente / prix_achat - 1) * 100;
     const gain = quantite_achetee * (1 + pourcentage / 100);
-
-
     const benefice = gain - quantite_achetee;
 
-    document.getElementById('pourcentage').innerHTML = "Pourcentage : " + pourcentage.toFixed(2) + " %";
-    document.getElementById('gain').innerHTML = "Gain " + gain.toFixed(2) + " ";
-    document.getElementById('benefices').innerHTML = "Bénéfice : " + benefice.toFixed(2);
-    if(benefice > 0) document.getElementById('benefices').style.color = "green";
-    else document.getElementById('benefices').style.color = "red";
+
+    // Affichage dans les labels
+    // Vérification que les valeurs sont non nuls pour pas remplir les labels alors que l'utilisateur
+    // n'a pas encore rempli entièrement le formulaire
+    if(prix_achat != "" && quantite_achetee != "" && prix_vente != ""){
+        document.getElementById('pourcentage').innerHTML = "Pourcentage : " + pourcentage.toFixed(2) + " %";
+        document.getElementById('gain').innerHTML = "Gain " + gain.toFixed(2) + " ";
+        document.getElementById('benefices').innerHTML = "Bénéfice : " + benefice.toFixed(2);
+        if(benefice >= 0) document.getElementById('benefices').style.color = "green";
+        else document.getElementById('benefices').style.color = "red";
+    }
 }
+
+
+// Attribution de la fonction action au changement des formulaires
+document.getElementById('prix_achat').onchange = action;
+document.getElementById('quantite_achetee').onchange = action;
+document.getElementById('prix_vente').onchange = action;
